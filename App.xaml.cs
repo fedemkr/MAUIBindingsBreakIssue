09ecc760405d77458a56f01ca46206a5d23ca134
Federico Maccaroni <fedemkr@gmail.com>
2023-09-01 14:04:43 -0300

    Binding issue

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BindingNullIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BindingNullIssue
+{
+    public class Person
+    {
+        public bool IsVisible { get; set; }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        Person _person;
+
+        public MainViewModel()
+		{
+		}
+
+        public string Title => "This is a title";
+
+        public Person Person => _person;
+
+        public bool IsTitleVisible => _person.IsVisible;
+
+        public bool IsTitleVisibleThatThrowsDirectly => throw new Exception("Whoops!");
+
+        public string Subtitle => "the subtitle"; // this won't be considered when binding IsTitleVisible or IsTitleVisibleThatThrowsDirectly but yes if binding Person.IsVisible
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
+
